8a0a66d41373893bdc085e22ba74a1eb72697587
daffa12ok <121938688+daffa12ok@users.noreply.github.com>
2025-04-30 18:55:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImage");
+
+document.querySelectorAll(".card img").forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function downloadImage(url) {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = url.split("/").pop();
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
